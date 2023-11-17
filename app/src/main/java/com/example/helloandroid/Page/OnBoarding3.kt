e30766526f05a133b7b7e67a835b11dc51069f23
sulthony.akbar <sulthonyakbar18@gmail.com>
2023-11-17 10:25:12 +0700

    add onboarding page

--- app/src/main/java/com/example/helloandroid/Page/OnBoarding3.kt
+++ app/src/main/java/com/example/helloandroid/Page/OnBoarding3.kt
@@ -1,6 +1,5 @@
 package com.example.helloandroid.Page
 
-import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,15 +13,12 @@ import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
